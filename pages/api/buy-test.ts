323de85cd9c1162ae52de2fa09f3e305fabb8cfc
DESKTOP-6N5G11B\admin <edwardmintel@gmail.com>
2024-11-21 03:40:14 -0800

    feat: api test

--- pages/api/buy-test.ts
+++ pages/api/buy-test.ts
@@ -1,7 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+const { CDP_API_KEY_PRIVATE_KEY, CDP_API_KEY_NAME } = process.env;
+
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({
-    status: "success"
+    private_key: CDP_API_KEY_PRIVATE_KEY,
+    key_name: CDP_API_KEY_NAME
   })
 }
 
