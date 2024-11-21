a94f1eb1037f6c3d0f113bea1d74b53998c0bc5c
DESKTOP-6N5G11B\admin <edwardmintel@gmail.com>
2024-11-21 03:48:18 -0800

    feat: solve api error

--- pages/api/buy-test.ts
+++ pages/api/buy-test.ts
@@ -3,7 +3,7 @@ const { CDP_API_KEY_PRIVATE_KEY, CDP_API_KEY_NAME } = process.env;
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({
-    private_key: CDP_API_KEY_PRIVATE_KEY,
+    private_key: CDP_API_KEY_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     key_name: CDP_API_KEY_NAME
   })
 }
